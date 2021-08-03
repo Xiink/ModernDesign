04cd9efc5041fee3b6d74e6c5aa5f0bc90b3c85f
Xiink <wwr774926@gmail.com>
2021-08-03 17:25:59 +0800

    加入專案檔案。

---
+++ ModernDesign/Core/ObservablrObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ModernDesign.Core {
+    class ObservablrObject : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media.Imaging;
+using ModernDesign.Core;
+
+namespace ModernDesign.MVVM.ViewModel {
+    class MainViewModel : ObservablrObject
+    {
+
+        public  RelayCommand HomeViewCommand { get; set; }
+
+        public  RelayCommand DSViewCommand { get; set; }
+
+        public HomeViewModel Homevm { get; set; }
+
+        public DiscoveryViewModel DSVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrenView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Homevm = new HomeViewModel();
+            DSVM = new DiscoveryViewModel();
+            CurrenView = Homevm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrenView = Homevm;
+            });
+
+            DSViewCommand = new RelayCommand(o => {
+                CurrenView = DSVM;
+            });
+        }
+    }
+}
